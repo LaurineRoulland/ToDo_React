55bd85a86f9647c89f852d4e0a8e2362dcd9e1b4
LaurineRoulland <l.roulland@hotmail.fr>
2023-04-22 18:09:37 +0200

    config checkbox

--- src/component/common/CheckboxControl.jsx
+++ src/component/common/CheckboxControl.jsx
@@ -1,5 +1,5 @@
 export const CheckboxControl = (props) => {
     return <div className="form-check">
-        <input className="form-check-input mx-1" type="checkbox" checked={props.isDone} onChange={props.handleClickDone} />
+        <input className="form-check-input mx-1" type="checkbox" checked={props.isDone || false} onChange={props.handleClickDone} />
     </div>
 }
